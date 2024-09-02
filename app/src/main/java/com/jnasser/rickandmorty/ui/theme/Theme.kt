80778d13afd6e16c3c52341e503d07e99c775058
jnasser <josenasser@latmobile.com>
2024-09-02 00:09:20 -0600

    Adding base theme and typography

--- app/src/main/java/com/jnasser/rickandmorty/ui/theme/Theme.kt
+++ app/src/main/java/com/jnasser/rickandmorty/ui/theme/Theme.kt
@@ -12,26 +12,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Green,
+    background = Black,
+    surface = DarkGray,
+    secondary = White,
+    tertiary = White,
+    primaryContainer = Green30,
+    onPrimary = Black,
+    onBackground = White,
+    onSurface = White,
+    onSurfaceVariant = Gray,
+    errorContainer = DarkRed5
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val LightColorScheme = DarkColorScheme
 
 @Composable
 fun RickAndMortyTheme(
